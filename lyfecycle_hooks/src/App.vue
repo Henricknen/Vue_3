6ac564c5d71878370f84fa8db4d1d4c41a643cd5
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-24 19:25:44 -0300

    'lyfecycle_hooks'_sao_eventos_que_acontece_em_determinada_etapa_da_aplicacao

---
+++ lyfecycle_hooks/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Estou codificando {{ codificando }}</p>    <!-- imprimindo na tela dados do componente -->
+    <LifeClycle/>
+  </div>
+</template>
+
+<script>
+  import LifeClycle from './components/LifeClycle.vue'    // importando componente 'LifeClycle'
+  export default {
+    name: 'App',
+    components: {
+      LifeClycle      // indica que este componente está sendo ultilizado
+    },
+    data() {
+      return {
+        codificando: "VueJs"
+      }
+    }
+  }
+</script>
