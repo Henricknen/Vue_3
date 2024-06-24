96a393da314ecfc526e74814d4f863d06993359a
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-23 23:52:58 -0300

    criando_um_'componente'_e_fazendo_sua_importacao

---
+++ componentes/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <primeiro_componente/>
+</template>
+
+<script>
+  import primeiro_componente from './components/primeiro_componente.vue'
+  export default {
+    name: 'App',
+    components: {
+      primeiro_componente
+    }
+  }
+</script>
