ad6aa9fbccc7e894644d76c69dec4b76304b24a9
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-25 12:23:20 -0300

    utilizando_diretivas_'v-if'_que_realiza_algo_de_a_condicao_for_true_'v-else'_realiza_algo_quando_a_condicao_do_v-if_nao_for_sastisfeita_e_'v-show'_que_unica_mostrar_ou_nao_mostra

---
+++ diretivas/src/components/Info.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <p v-if = "estou_trabalhando">Estou trabalhando no momento</p>       <!-- utilizando diretiva 'v-if' para indicar de estou ou não estou trabalhando -->
+        <p v-else>Estou em busca de novas oportunidades</p>     <!-- esse paragrafo será apresentado se o v-if não for sastifeita -->
+        <p>Sou um profissional que tem habilidades de desenvolvimento do lado do cliente quanto do lado do servidor</p>
+        <p>Utilizo as seguintes tecnologias</p>
+        <ul>
+            <li>JavaScript</li>
+            <li>Python</li>
+            <li>PHP</li>
+            <li>Sql</li>
+            <p v-show = "mostrar_email">Mande uma menssagem para: {{ email }}</p>       <!-- diretiva 'v-show' mostra o email enquanto a propriedade for true -->
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Info',
+    data() {
+        return {
+            estou_trabalhando: false,     // só será exibido se o valor for 'true'
+            mostrar_email: true,
+            email: 'l.henrick@live.com'
+        }
+    }
+}
+</script>
