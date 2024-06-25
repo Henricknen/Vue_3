6b1a508850ebb985e3433ade168501f7eea3ce45
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-24 21:44:21 -0300

    criando_componetes_e_fazendo_suas_'reultilizacao'_em_outras_partes_do_sistema

---
+++ hierarquia_de_componentes/src/components/Info.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>Sou um profissional que tem habilidades de desenvolvimento do lado do cliente quanto do lado do servidor</p>
+        <p>Utilizo as seguintes tecnologias</p>
+        <ul>
+            <li>JavaScript</li>
+            <li>Python</li>
+            <li>PHP</li>
+            <li>Sql</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Info'
+}
+</script>
---
+++ hierarquia_de_componentes/src/components/Pessoa.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Está é a descrição de: {{ nome }}</h2>
+        <Info />        <!-- importando componente 'Info' -->
+        <Info />            <!-- replicando componente -->
+        <Form />
+    </div>
+</template>
+
+<script>
+    import Info from './Info'
+    import Form from './Form'
+    export default {
+        name: 'Pessoa',
+        components: {
+            Info,        // reutilizando componente importado
+            Form
+        },
+        data() {
+            return {
+                nome: 'Luis Henrique S. F.'
+            }
+        }
+    }
+</script>
