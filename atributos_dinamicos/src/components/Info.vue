530b455a881d8c69e8dcd2cbe2c651b9c100e077
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-25 13:39:47 -0300

    ultilizando_valores_dinamicos_chamados_de_'argumento'_para_alterar_link_de_uma_tag_'a'_e_inseri_uma_imagem

---
+++ atributos_dinamicos/src/components/Info.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <p v-if = "estou_trabalhando">Estou trabalhando no momento</p>
+        <p v-else>Estou em busca de novas oportunidades</p>
+        <p>Sou um profissional que tem habilidades de desenvolvimento do lado do cliente quanto do lado do servidor</p>
+        <p>Utilizo as seguintes tecnologias</p>
+        <ul>
+            <li>JavaScript</li>
+            <li>Python</li>
+            <li>PHP</li>
+            <li>Sql</li>
+            <p v-show = "mostrar_email">Mande uma menssagem para: {{ email }}</p>
+            <p>Acessar meu portifólio <a v-bind:href = "meu_link">Click aki</a></p>     <!-- atributo 'v-bind' interpreta o que está na propriedade meu_link como um dado dinâmico -->
+            <Picture />
+        </ul>
+    </div>
+</template>
+
+<script>
+import Picture from './Picture.vue'
+export default {
+  components: { Picture },
+    name: 'Info',
+    components: {
+        Picture
+    },
+    data() {
+        return {
+            estou_trabalhando: false,
+            mostrar_email: true,
+            email: 'l.henrick@live.com',
+            meu_link: 'https://portfoli0henricknen.netlify.app/'        // atributo 'meu_link' contém o link do meu portifólio
+        }
+    }
+}
+</script>
---
+++ atributos_dinamicos/src/components/LifeClycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Sou um profissional: {{ hardskill }}</h1>        <!-- ultilizando propriedade 'nome' -->
+</template>
+
+<script>
+export default {
+    name: 'LyfeCycle',    
+    data() {
+        return {
+            hardskill: 'Back End...'       // criando dado 'Full Stack...' em propriedade nome
+        }
+    },
+    created() {     // novo item 'created'
+      setTimeout(()=> {   // função 'setTimeout' condiciona a troca após um certo tempo definido
+        this.hardskill = "Front End..."
+      }, 1000)
+    },
+    mounted() {
+      setTimeout(()=> {
+        this.hardskill = "Full Stack..."
+      }, 2000)
+    }
+}
+</script>
