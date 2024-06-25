d049d4e40bd689aa466887caaeeed7343b9a2dd8
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-25 20:12:35 -0300

    criando_'metodo'_e_associando_ao_evento_'showEmail'_que_sera_acionado_pela_diretiva_'@click'_do_botao

---
+++ metodos/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>Estou codificando {{ codificando }}</p>
+    <LifeClycle />
+    <Pessoa />    <!-- importando componente 'Pessoa' -->
+  </div>
+</template>
+
+<script>
+  import LifeClycle from './components/LifeClycle.vue'
+  import Pessoa from './components/Pessoa.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      LifeClycle,
+      Pessoa
+    },
+    data() {
+      return {
+        codificando: "VueJs"
+      }
+    }
+  }
+</script>
---
+++ metodos/src/components/Picture.vue
@@ -0,0 +1,15 @@
+<template>
+    <img :src = "framework" :alt = "descricao">     <!-- sintaxe resumida do v-bind é utilizar ':' antes de src de de alt -->
+</template>
+
+<script>
+export default {
+    name: 'Picture',
+    data() {
+        return {
+            framework: "/img/vue3.png",
+            descricao: "Imagem do framework utilizado nesta codificação"
+        }
+    }
+}
+</script>
