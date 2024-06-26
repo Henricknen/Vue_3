b23977f75887b0bbae5791b6e68914aa867a3757
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-26 19:29:42 -0300

    criando_css_especificos_'scoped'_em_barra_de_navegacao_que_substitui_estilos_inseridos_pelo_css_'global'_que_e_aplicado_a_todos_os_elementos

---
+++ css-scoped_css-global/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <Header />    <!-- importação do componente 'Header' -->
+    <p class = "paragrafo_pai">Estou codificando {{ codificando }}</p>    <!-- fazendo formatação 'css-global' utilizando classe paragrafo_pai -->
+    <LifeClycle />
+    <Pessoa />    <!-- importando componente 'Pessoa' -->
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'    // importando componente 'Header'
+  import LifeClycle from './components/LifeClycle.vue'
+  import Pessoa from './components/Pessoa.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Header,   // chamando componente 'Header'
+      LifeClycle,
+      Pessoa
+    },
+    data() {
+      return {
+        codificando: "VueJs"
+      }
+    }
+  }
+</script>
+
+<style>
+  body {    /* css global */
+    background-color: #333;
+    color: #FFF;
+  }
+
+  a {
+    color: red;
+  }
+
+  .classe {
+    background-color: #000;
+  }
+</style>
---
+++ css-scoped_css-global/src/components/Info.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <p v-if = "estou_trabalhando">Estou trabalhando no momento</p>
+        <p v-else>Estou em busca de novas oportunidades</p>
+        <p>Sou um profissional que tem habilidades de desenvolvimento do lado do cliente quanto do lado do servidor</p>
+        <p>Utilizo as seguintes tecnologias</p>
+        <ul>
+            <li>JavaScript</li>
+            <li>Python</li>
+            <li>PHP</li>
+            <li>Sql</li>
+        </ul>
+        <div>
+            <button @click = "showEmail">{{ texto_botao }}</button>        <!-- botão ultilizando diretiva '@click' que ativa o método por meio de um click -->
+        </div>
+            <p v-show = "mostrar_email">Mande uma menssagem para: {{ email }}</p>
+            <p class = "classe">Acessar meu portifólio <a v-bind:href = "meu_link">Click aki</a></p>
+            <Picture />
+    </div>
+</template>
+
+<script>
+import Picture from './Picture.vue'
+export default {
+  components: { Picture },
+    name: 'Info',
+    components: {
+        Picture
+    },
+    data() {
+        return {
+            estou_trabalhando: false,
+            mostrar_email: false,
+            email: 'l.henrick@live.com',
+            meu_link: 'https://portfoli0henricknen.netlify.app/',
+            texto_botao: 'Mostrar e-mail'       // texto do botão
+        }
+    },
+    methods: {
+        showEmail() {       // quando botão for clicado este método será açionado
+            this.mostrar_email = !this.mostrar_email        // o método inverte o valor de 'mostrar_email' deixando 'true' o apresentando na tela
+            if(!this.mostrar_email) {       // if 'troca' a menssagem do botão
+                this.texto_botao = 'Mostrar e-mail'
+            } else {
+                this.texto_botao = 'Esconder e-mail'
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    .paragrafo_pai {
+        color: red;
+    }
+</style>
