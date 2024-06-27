b381d7e592e9f1b2a7520a1222251b8c530d1092
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-26 23:56:40 -0300

    criando_'evento'_para_executar_acoes_do_usuario_que_dipara_um_metodo

---
+++ eventos/src/components/Form.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h2>Meu formulário</h2>
+        <form action="" @submit = "enviar_formulario($event)">
+            <div>
+                <input type = "text" v-model = "nome">
+            </div>
+            <div>
+                <input type = "text" v-model = "email">              
+            </div>
+            <div>
+                <Submit />
+            </div>
+        </form>
+        <p>Conteúdo</p>
+        <p>Conteúdo</p>
+        <p>Conteúdo</p>
+        <p>Conteúdo</p>
+    </div>
+</template>
+
+<script>
+import InputText from './form/inputText.vue'
+import Submit from './form/Submit.vue'
+
+export default {
+    name: 'Form',
+    components: {
+        InputText,
+        Submit
+    },
+    data() {
+        name: "Luis Henrique S F";
+        email: "l.henrick@live.com";
+    },
+    methods: {
+        enviar_formulario(e) {      // 'e' previne que o formulário seja enviado
+            e.preventDefault();
+
+            const name = this.name;
+            const email = this.email;
+
+            console.log("O nome é: " + name);
+            console.log("O email é: " + email);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    p {
+        margin: 150px;
+    }
+</style>
---
+++ eventos/src/components/Pessoa.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>Está é a descrição de: {{ nome }}</h2>
+        <Info />        <!-- importando componente 'Info' -->
+        <Form />
+    </div>
+</template>
+
+<script>
+    import Info from './Info'
+    import Form from './Form'
+    export default {
+        name: 'Pessoa',
+        components: {
+            Info,        // reutilizando componente importado
+            Form
+        },
+        data() {
+            return {
+                nome: 'Luis Henrique S. F.'
+            }
+        }
+    }
+</script>
